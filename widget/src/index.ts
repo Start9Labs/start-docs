b21c8129af13e69d3d3c9497dc367705440f0b0f
Matt Hill <mattnine@protonmail.com>
2026-02-18 20:44:18 -0700

    change icon

--- widget/src/index.ts
+++ widget/src/index.ts
@@ -260,8 +260,12 @@ class DocsAgentWidget {
   private renderClosed(): string {
     return `
       <button class="da-fab" aria-label="Open support chat">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/>
+          <path d="M5 3v4"/>
+          <path d="M3 5h4"/>
+          <path d="M19 17v4"/>
+          <path d="M17 19h4"/>
         </svg>
       </button>
     `;
